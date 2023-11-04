6b0c834ef43a89a1e4ca41f3f0bef4d0ba7fa621
Kit Cox <chriscox@abandonedfactory.net>
2023-11-04 00:43:31 -0700

    Adding AllItems view

--- service/web.go
+++ service/web.go
@@ -31,3 +31,26 @@ func (svc *Service) WebListLocations(w http.ResponseWriter, r *http.Request, _ h
 		slog.Error("failed to render template", "error", err.Error())
 	}
 }
+
+func (svc *Service) WebAllItems(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	viewData := struct {
+		Items []library.Item
+	}{}
+	//params := httprouter.ParamsFromContext(r.Context())
+	items, err := svc.LibraryStorage.GetAllItems(r.Context())
+	if err != nil {
+		slog.Error("failed to fetch items", "err", err)
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	viewData.Items = items
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	//w.Header().Set("Content-Encoding", "utf-8")
+	err = svc.htmlTemplates.ExecuteTemplate(w, "ListItems.html", viewData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		slog.Error("failed to render template", "error", err.Error())
+	}
+}
