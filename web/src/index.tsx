12b3577a8979d3353ff162aec921556fe3193c51
Kit Cox <chriscox@abandonedfactory.net>
2023-11-09 01:51:03 -0800

    Adding New Item Page. Create New Location function working

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import LocationsPage from "./pages/LocationsPage";
+import {ItemsPage} from "./pages/ItemsPage";
+import {AddItemPage} from "./pages/AddItemPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LocationsPage />
+    },
+    {
+        path: "/locations",
+        element: <LocationsPage />
+    },
+    {
+        path: "/items",
+        element: <ItemsPage />
+    },
+    {
+        path: "/add",
+        element: <AddItemPage />
+    }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
